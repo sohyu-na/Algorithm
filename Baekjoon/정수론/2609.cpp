30f29b90602141ea255f94eaddd702b2217eae4f
SoHyunA <solim73@naver.com>
2024-03-01 20:04:11 +0900

    [solved] 2609 최대공약수와 최소공배수

--- Baekjoon/정수론/2609.cpp
+++ Baekjoon/정수론/2609.cpp
@@ -2,17 +2,32 @@
 
 using namespace std;
 
+int getGCD(int a, int b){
+    while(b){
+        a%=b;
+        swap(a,b);
+    }
+    return a;
+}
 int main()
 {
     int a,b; cin >> a >> b;
+    /*
     int l = a > b ? a : b;
     int s = a > b ? b : a;
-
+    
     while (s) {
         l %= s;
         swap(l, s);
     }
     cout <<l<<'\n';
     cout << a * b / l;
+    */
+   if(b>a)
+        swap(a,b);
+    int gcd = getGCD(a,b);
+    
+    cout << gcd <<"\n";
+    cout << a*b/gcd;
 
 }
