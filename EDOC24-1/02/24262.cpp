b69c869b553767513b02b7ec5ac63e93b3f9c0bd
SoHyunA <solim73@naver.com>
2024-03-28 19:40:58 +0900

    [solved] 24262 알고리즘 수업

---
+++ EDOC24-1/02/24262.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n; cin >> n;
+    cout << "1\n";
+    cout << "0";
+    
+    return 0;
+    
+}
+
+
