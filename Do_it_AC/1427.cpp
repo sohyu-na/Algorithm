207fd49dd8118c8f936e99db1780029933d8164d
SoHyunA <solim73@naver.com>
2024-04-08 19:08:48 +0900

    [017] 내림차순으로 자릿수 정렬하기

---
+++ Do_it_AC/1427.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+int main()
+{
+    int n; cin >> n ;
+    multiset <int> s;
+   
+    for (int i = 0; i < 10; i++) {
+        s.insert(n % 10);
+        if (n / 10 == 0)
+            break;
+        else
+            n = n / 10;
+    }
+
+    for (auto iter=s.rbegin();iter!=s.rend();iter++) {
+        cout << *iter;
+    }    
+    return 0;
+}
