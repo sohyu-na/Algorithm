a38f1b45da9b4e0ab45afc89f508b706fa8ccb2b
sohyu-na <solim73@naver.com>
2024-10-09 16:27:37 +0900

    [083] 선물 전달하기

---
+++ Do_it_AC/1947.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int n; cin >> n;
+
+    long long dp[1000001];
+    dp[1] = 0;
+    dp[2] = 1;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = ((i - 1)*(dp[i - 2] + dp[i - 1]))%1000000000;
+    }
+    
+    cout << dp[n];
+  
+    return 0;
+}
