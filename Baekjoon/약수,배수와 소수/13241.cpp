190c7d61bfb0266d84fe91bfae163e8876a09bab
sohyu-na <solim73@naver.com>
2026-03-31 15:09:57 +0900

    [solved] 13241 최소공배수

---
+++ Baekjoon/약수,배수와 소수/13241.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <climits>
+
+using namespace std; 
+
+int gcd(long long a, long long b) {
+	if (b == 0) {
+		return a;
+	}
+	return gcd(b, a % b);
+	
+}
+
+int main() {
+	long long a, b; cin >> a >> b;
+	
+	cout<< a*b/gcd(a, b);
+	return 0;
+}
+
+	
